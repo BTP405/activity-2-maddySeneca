a5e0999ff49b702808368b3a84c64f24cad6d84a
maddySeneca <158625426+maddySeneca@users.noreply.github.com>
2024-02-14 20:06:20 -0500

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,1 +1,23 @@
+def checkPrime(num):
+     if num < 2:
+        return False
+     for i in range(2, int(num*0.5) + 1):
+        if num % i == 0:
+            return False
+     return True
+
 def getPrimeNumbers(n):
+    primeNumbers = [];
+    for i in range(2,n):
+        if checkPrime(i):
+            primeNumbers.append(i);
+    
+    return primeNumbers;
+
+primeList = getPrimeNumbers(20);
+print(primeList)
+
+#here i am first using the check prime function to check if a number is prime or not and then simply running the function
+# again and again in the getPrimeNumbers function
+# The checkPrime function simply checks if the number is divisible by 
+# any number between the range 2 to n/2
