3b148f02726c2a03fadc24f59d09237c09daaccf
maddySeneca <158625426+maddySeneca@users.noreply.github.com>
2024-02-14 20:06:44 -0500

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,1 +1,20 @@
+import matplotlib.pyplot as plt
+
 def graphSnowfall(t):
+    with open(t, 'r') as file:
+        snowfall_data = [int(line.strip()) for line in file]
+
+    ranges = [0, 10, 20, 30, 40, 50]
+    aggregated_data = []
+    for r in ranges:
+        count = 0
+        for value in snowfall_data:
+            if r < value <= r + 10:
+                count += 1
+        aggregated_data.append(count)
+
+    plt.bar([f'{r+1}-{r+10}cms' for r in ranges], aggregated_data)
+    plt.xlabel('Snowfall Range')
+    plt.ylabel('Number of Occurrences')
+    plt.title('Snowfall Accumulation')
+    plt.show()
